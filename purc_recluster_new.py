ef73cf0fd5532f0605de16d1f5055682374f5e05
Fay-Wei Li <fayweili@gmail.com>
2016-02-07 10:38:38 -0800

    sort by size, instead of length in the final clustering step
    Former-commit-id: bd085a0c7870851651274997d55a1f6ddf7b95d1

--- purc_recluster_new.py
+++ purc_recluster_new.py
@@ -375,7 +375,7 @@ def ClusterDechimera(annotd_seqs_file, clustID, clustID2, clustID3, sizeThreshol
 					shutil.copyfileobj(open(file,'rb'), all_consensus_seq) #Add each file to the final output
 				all_consensus_seq.close()
 
-				usearch_cline = "%s -sortbylength %s -fastaout %s" %(Usearch, bcode_folder + '_Cluster_Finalconsensus.fa', bcode_folder + '_Cluster_FinalconsensusSs.fa')
+				usearch_cline = "%s -sortbysize %s -fastaout %s" %(Usearch, bcode_folder + '_Cluster_Finalconsensus.fa', bcode_folder + '_Cluster_FinalconsensusSs.fa')
 				process = subprocess.Popen(usearch_cline, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)	
 				(out, err) = process.communicate() #the stdout and stderr
 				
